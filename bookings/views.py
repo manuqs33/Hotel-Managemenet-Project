ba89f47dcf52874ec476e9fac3ca40bbdd00d809
manu <you@example.com>
2023-01-20 14:38:20 -0300

    Enable only the needed HTTP methods in each ViewSet

--- bookings/views.py
+++ bookings/views.py
@@ -10,25 +10,11 @@ class BookingViewSet(ModelViewSet):
 
     serializer_class = BookingSerializer
     queryset = Booking.objects.all()
-    """ permission_classes = [DjangoObjectPermissions] """
-
-    """ def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save(serializer)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def list(self, request):
-        serializer = self.get_serializer(self.get_queryset(), many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk):
-        item = self.get_object()
-        serializer = self.get_serializer(item)
-        return Response(serializer.data) """
+    http_method_names = ['get', 'post', 'put']
 
 
 class RoomViewSet(ModelViewSet):
     serializer_class = RoomSerializer
     queryset = Room.objects.all()
+    http_method_names = ['get']
 
